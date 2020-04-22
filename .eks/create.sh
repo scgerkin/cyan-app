03cb3fa4c99ac5a980fcc513af3abab2e9225a8d
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-22 00:05:56 +0000

    fix

--- .eks/create.sh
+++ .eks/create.sh
@@ -4,12 +4,12 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "${RED}Creating cluster...${NC}"
-eksctl create cluster -f stack.yaml
+eksctl create cluster -f .eks/stack.yaml
 echo -e "${RED}Creating LoadBalancer Service...${NC}"
-kubectl apply -f templates/svc-lb.yaml
+kubectl apply -f .eks/svc-lb.yaml
 echo -e "${RED}Creating Replication Controllers...${NC}"
-kubectl apply -f templates/rc-blue.yaml
-kubectl apply -f templates/rc-green.yaml
+kubectl apply -f .eks/rc-blue.yaml
+kubectl apply -f .eks/rc-green.yaml
 
 echo -e "${RED}Listing clusters...${NC}"
 eksctl get clusters --region=us-east-2
